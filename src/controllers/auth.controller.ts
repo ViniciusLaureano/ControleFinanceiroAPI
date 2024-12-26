e41fc788d3cc347f5d9591e25b9a2a32c7d55209
ViniciusLaureano <viniciusglaureano@gmail.com>
2024-12-26 11:19:17 -0300

    docs(auth): document controller and system docs

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthRegisterDTO } from 'src/dtos/auth/auth.register.dto';
 import { AuthService } from 'src/services/auth.service';
 
-@Controller()
+@ApiTags('Auth')
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({
+    summary: 'Create an User',
+    description: 'Create a new user',
+  })
   register(@Body() AuthRegisterDTO: AuthRegisterDTO): Promise<AuthRegisterDTO> {
     return this.authService.register(AuthRegisterDTO);
   }
--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,9 @@ async function bootstrap() {
     .setTitle('API Example')
     .setDescription(
       `Initial API base for the 'ControleFinanceiro' project.
-For all paths (with the exception of 'GET: /' and 'POST: /auth/login'), a Barear Token is required to execute the request.`,
+For all paths (with the exception of 'GET: /', 'POST: /auth/login' and 'POST: /auth/register'), a Barear Token is required to execute the request.`,
     )
     .setVersion('1.0')
-    .addTag('Base')
     .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
