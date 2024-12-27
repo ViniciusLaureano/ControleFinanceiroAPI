3db9e8f0164b2abca7d6f25672ad7c4f5ad9a145
ViniciusLaureano <viniciusglaureano@gmail.com>
2024-12-27 09:49:58 -0300

    feat(roles): implement `@Roles()`

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PrismaService } from './database/prisma.service';
 import { AuthModule } from './modules/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './guards/jwt.auth.guard';
+import { RolesGuard } from './guards/roles.guard';
 
 @Module({
   imports: [AuthModule],
@@ -15,6 +16,10 @@ import { JwtAuthGuard } from './guards/jwt.auth.guard';
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
     },
   ],
 })
