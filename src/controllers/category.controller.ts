3fa16a84bf109242574f059b506ce579265d512b
ViniciusLaureano <viniciusglaureano@gmail.com>
2024-12-29 19:33:59 -0300

    docs(category): swagger routes config

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -3,25 +3,33 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Category } from '@prisma/client';
 import { CategoryCreateDTO } from 'src/dtos/category/category.create.dto';
 import { CategoryUserDTO } from 'src/dtos/category/category.user.dto';
 import { CategoryService } from 'src/services/category.service';
 
+@ApiTags('Category')
 @Controller('category')
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
 
   @Get('')
+  @HttpCode(HttpStatus.OK)
+  @ApiBearerAuth('access-token')
   getCategories(@Body() categoryUserDTO: CategoryUserDTO): Promise<Category[]> {
     return this.categoryService.getCategories(categoryUserDTO.userId);
   }
 
   @Get('total')
+  @HttpCode(HttpStatus.OK)
+  @ApiBearerAuth('access-token')
   getTotalActivesCategories(
     @Body() categoryUserDTO: CategoryUserDTO,
   ): Promise<number> {
@@ -31,16 +39,22 @@ export class CategoryController {
   }
 
   @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  @ApiBearerAuth('access-token')
   getCategoryById(@Param('id') id: string): Promise<Category> {
     return this.categoryService.getCategoryById(id);
   }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  @ApiBearerAuth('access-token')
   deleteCategory(@Param('id') id: string): Promise<Category> {
     return this.categoryService.deleteCategory(id);
   }
 
   @Put(':id')
+  @HttpCode(HttpStatus.OK)
+  @ApiBearerAuth('access-token')
   editCategory(
     @Param('id') id: string,
     @Body() body: CategoryCreateDTO,
@@ -49,6 +63,8 @@ export class CategoryController {
   }
 
   @Post('')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiBearerAuth('access-token')
   createCategory(@Body() body: CategoryCreateDTO): Promise<Category> {
     return this.categoryService.createCategory(body);
   }
