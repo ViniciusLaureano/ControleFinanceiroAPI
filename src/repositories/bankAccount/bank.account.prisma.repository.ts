0f89c5effd2826509c83cb1d4b64da0e843301c4
ViniciusLaureano <viniciusglaureano@gmail.com>
2024-12-31 22:20:14 -0300

    feat(bankAccount): define repository methods and prisma actions

--- src/repositories/bankAccount/bank.account.prisma.repository.ts
+++ src/repositories/bankAccount/bank.account.prisma.repository.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { BankAccountRepository } from './bank.account.repository';
 import { PrismaService } from 'src/database/prisma.service';
+import { Bank_Account, BankAccountType } from '@prisma/client';
 
 @Injectable()
 export class PrismaBankAccountRepository implements BankAccountRepository {
   constructor(private prisma: PrismaService) {}
+  getAllBankAccountsUser(user_id: string): Promise<Bank_Account[]> {
+    return this.prisma.bank_Account.findMany({ where: { user_id } });
+  }
+
+  getBankAccountById(id: string): Promise<Bank_Account> {
+    return this.prisma.bank_Account.findUnique({ where: { id } });
+  }
+
+  createBankAccount(
+    name: string,
+    type: BankAccountType,
+    bank: string,
+    initial_value: number,
+    user_id: string,
+  ): Promise<Bank_Account> {
+    return this.prisma.bank_Account.create({
+      data: { name, type, bank, initial_value, user_id },
+    });
+  }
+
+  updateBankAccount(
+    id: string,
+    name: string,
+    type: BankAccountType,
+    bank: string,
+    initial_value: number,
+  ): Promise<Bank_Account> {
+    return this.prisma.bank_Account.update({
+      where: { id },
+      data: { name, type, bank, initial_value },
+    });
+  }
+
+  deleteBankAccount(id: string): Promise<Bank_Account> {
+    return this.prisma.bank_Account.delete({ where: { id } });
+  }
 }
--- src/repositories/bankAccount/bank.account.repository.ts
+++ src/repositories/bankAccount/bank.account.repository.ts
@@ -1,1 +1,25 @@
-export abstract class BankAccountRepository {}
+import { Bank_Account, BankAccountType } from '@prisma/client';
+
+export abstract class BankAccountRepository {
+  abstract getAllBankAccountsUser(user_id: string): Promise<Bank_Account[]>;
+
+  abstract getBankAccountById(id: string): Promise<Bank_Account>;
+
+  abstract createBankAccount(
+    name: string,
+    type: BankAccountType,
+    bank: string,
+    initial_value: number,
+    user_id: string,
+  ): Promise<Bank_Account>;
+
+  abstract updateBankAccount(
+    id: string,
+    name: string,
+    type: BankAccountType,
+    bank: string,
+    initial_value: number,
+  ): Promise<Bank_Account>;
+
+  abstract deleteBankAccount(id: string): Promise<Bank_Account>;
+}
